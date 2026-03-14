7189ee2c803d2e9d38182b1bf97b2ceb56e4ac4e
Alex García A. <alex.garcia@grupoalmar.com.ec>
2026-03-14 13:28:29 -0500

    first commit

---
+++ lib/presentation/screens/chat_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+              'https://i.insider.com/5b801ab63cccd12d008b4590?width=700',
+            ),
+          ),
+        ),
+        title: const Text('Mi amor ❤️'),
+        centerTitle: false,
+      ),
+      body: const Center(child: Text('Welcome to the Chat Screen!')),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  const _ChatView();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(),
+      ),
+    );
+  }
+}
